9e5497c0bca893af6a737b8c6f670e8eebd30b42
Freely-TEA <pticavruke@gmail.com>
2020-10-30 13:44:21 +0700

    Осталась привязка к боту

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
-import logging
-import aiogram
-import asyncio
-url = "https://www.playground.ru/news"
-resp = requests.get(url)
-resp = resp.text
-soup = BeautifulSoup(resp, 'html.parser')
-tag = "time"
-source = "datetime"
-ide = ""
-pr = soup.findAll(tag, source = ide)
-for el in pr:
-    print(el)
+
+ar = ""
+print(ar.split())
